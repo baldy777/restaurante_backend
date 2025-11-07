0aa70077a0a74a0c6f60c630daada09097ffdd8e
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-06 23:10:46 -0400

    feat: adicion de funcion de puertos y comprobacion de estado del servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo! Servidor funcionando correctamente.');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Servidor levantado gracias a Diego en el puerto 3000');
+});
